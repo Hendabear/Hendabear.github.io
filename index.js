a16e1bcadf4802a1b0e546455ab57968431a8905
Collin Henderson <collinfhenderson@gmail.com>
2020-08-30 12:32:19 -0400

    Creature Table, Additional Dice Roll, Combine Creature & Terrain
    Added creature encounter table then added functionality to combine creature and terrain. Added second dice roll.

--- index.js
+++ index.js
@@ -43,22 +43,57 @@ function terrainEncounters(roll) {
 }
 
 function randomEncounters(roll) {
+	let diceNumber = getRandomIntInclusive(1, 20);
+	let secondDiceNumber = getRandomIntInclusive(1, 20);
 	if(roll < 14) {
 		// noEncounter
 		return 'No Encounter';
 	}
 	else if(roll === 14 || roll === 15 ) {
-		let diceNumber = getRandomIntInclusive(1, 20);
   		return terrainEncounters(diceNumber);
 	}
 	else if(roll === 16 || roll ===17) {
   		// one or more creatures
-  		return 'Creature Encounter';
+  		return creatureEncounters(diceNumber);
 	}
   	else {
-  		let diceNumber = getRandomIntInclusive(1, 20);
-  		return terrainEncounters(diceNumber);
+ 		let monster = creatureEncounters(secondDiceNumber);
+  		let place = terrainEncounters(diceNumber);
+  		return `${monster} in ${place}`;
   		// terrain encounter featuring one or more creatures
   	}
 
 }
+
+function creatureEncounters(roll) {
+	switch (true) {
+		case roll === 1 || roll === 2 :
+			return 'Ambushers';
+			// NOTE address that we need to reroll if characters are resting
+		case roll === 3 :
+			return 'Carrion Crawler';
+		case roll === 4 || roll === 5 :
+			return 'Escaped Slaves';
+		case roll === 6 || roll === 7 :
+			return 'Fungi';
+		case roll === 8 || roll === 9 :
+			return 'Giant Fire Beetles';
+		case roll === 10 || roll === 11 :
+			return 'Giant "Rocktopus"';
+		case roll === 12 :
+			return 'Mad Creature';
+		case roll === 13 :
+			return 'Ochre Jelly';
+		case roll === 14 || roll === 15 :
+			return 'Raiders';
+		case roll === 16 :
+			return 'Scouts';
+		case roll === 17 :
+			return 'Society of Brilliance';
+		case roll === 18 :
+			return 'Spore Servants';
+
+		default:
+			return 'Traders';
+	}
+} 
